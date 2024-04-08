38c558fe7361b7792999751e6fb161444b38560c
theekhaR <60611475+theekhaR@users.noreply.github.com>
2024-04-08 19:48:02 +0700

    Add files via upload

---
+++ lib/chat.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+import 'package:project1/CreatePost.dart';
+import 'package:project1/homepage.dart';
+import 'package:project1/message.dart';
+import 'package:project1/noti1.dart';
+import 'package:project1/dot_navigation_bar.dart';
+import 'package:project1/profile.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+
+enum _SelectedTab { Home, AddPost, Chat, Profile } // Nav bar
+
+class Chat extends StatefulWidget {
+  const Chat({Key? key}) : super(key: key);
+
+  @override
+  State<Chat> createState() => _Chat();
+}
+
+class _Chat extends State<Chat> {
+  var _selectedTab = _SelectedTab.Chat; // Nav bar
+  void _handleIndexChanged(int i) {
+    // Nav bar
+    setState(() {
+      _selectedTab = _SelectedTab.values[i];
+      if (_selectedTab == _SelectedTab.Home) {
+        // Navigate to Homepage
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      } else if (_selectedTab == _SelectedTab.Profile) {
+        // Navigate to Profile
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const FormPage()),
+        );
+      } else if (_selectedTab == _SelectedTab.Chat) {
+        // Navigate to Chat
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const Chat()),
+        );
+      } else if (_selectedTab == _SelectedTab.AddPost) {
+        // Navigate to CreatePost
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const CreatePostPage()),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // AppBar
+      appBar: AppBar(
+        // Block
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            color: const Color.fromARGB(255, 250, 86, 114),
+            icon: const Icon(Icons.notifications),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SettingsPage2()),
+              );
+            },
+          ),
+        ],
+        // Line
+        //elevation: 0,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(12), //space between text and line
+          child: Container(
+            height: 2.0, //Line Size
+            color: const Color.fromARGB(255, 250, 86, 114),
+          ),
+        ),
+        // Text of Appbar
+        title: Text(
+          'Chat',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+            color: Color(0xFF26117A),
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chat')
+            .orderBy('datetime', descending: true)
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((doc) => doc.data()).toList()),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (snapshot.hasData) {
+            final chatData = snapshot.data!;
+            return ListView.builder(
+              itemCount: chatData.length,
+              itemBuilder: (context, index) {
+                final _message = chatData[index];
+                return Column(
+                  children: <Widget>[
+                    Divider(
+                      height: 1.0,
+                    ),
+                    GestureDetector(
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          radius: 24.0,
+                          backgroundImage: AssetImage(
+                              _message['avatar']), // assuming avatar is a path
+                        ),
+                        title: Row(
+                          children: <Widget>[
+                            Text(
+                              _message['name'],
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                            SizedBox(
+                              width: 16.0,
+                            ),
+                            Text(
+                              _formatTimestamp(
+                                _message['datetime'],
+                              ),
+                              style: TextStyle(fontSize: 15.0),
+                            ),
+                          ],
+                        ),
+                        subtitle: Text(
+                          _message['message'],
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                Message(chatID: _message['chatID']),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      bottomNavigationBar: SizedBox(
+        // Nav bar
+        height: 160,
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 0),
+          child: DotNavigationBar(
+            margin: const EdgeInsets.only(left: 30, right: 30),
+            currentIndex: _SelectedTab.values.indexOf(_selectedTab),
+            dotIndicatorColor: const Color.fromARGB(255, 250, 86, 114),
+            unselectedItemColor: Colors.grey[300],
+            splashBorderRadius: 50,
+            //enableFloatingNavBar: false,
+            onTap: _handleIndexChanged,
+            items: [
+              /// Home
+              DotNavigationBarItem(
+                icon: const Icon(Icons.home),
+                selectedColor: const Color.fromARGB(255, 250, 86, 114),
+              ),
+
+              /// Likes
+              DotNavigationBarItem(
+                icon: const Icon(Icons.add_circle),
+                selectedColor: const Color.fromARGB(255, 250, 86, 114),
+              ),
+
+              /// Search
+              DotNavigationBarItem(
+                icon: const Icon(Icons.chat),
+                selectedColor: const Color.fromARGB(255, 250, 86, 114),
+              ),
+
+              /// Profile
+              DotNavigationBarItem(
+                icon: const Icon(Icons.person),
+                selectedColor: const Color.fromARGB(255, 250, 86, 114),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+String _formatTimestamp(Timestamp timestamp) {
+  DateTime dateTime = timestamp.toDate();
+  return DateFormat.jm().format(dateTime);
+}
---
+++ lib/message.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+
+class Message extends StatefulWidget {
+  final int chatID;
+
+  const Message({Key? key, required this.chatID}) : super(key: key);
+
+  @override
+  _MessageState createState() => _MessageState();
+}
+
+class _MessageState extends State<Message> {
+  final TextEditingController _messageController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('chat')
+              .doc(widget.chatID.toString())
+              .collection('messages')
+              .orderBy('datetime', descending: false)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Text(
+                'Loading...',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                  color: Color(0xFF26117A),
+                ),
+              );
+            }
+
+            final messages = snapshot.data!.docs;
+            final firstMessage = messages.first.data() as Map<String, dynamic>;
+            final receiverName =
+                firstMessage['receipt'] ? 'Chat' : firstMessage['name'];
+
+            return Text(
+              receiverName,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+                color: Color(0xFF26117A),
+              ),
+            );
+          },
+        ),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(12),
+          child: Container(
+            height: 2.0,
+            color: const Color.fromARGB(255, 250, 86, 114),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(top: 20.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('chat')
+                    .doc(widget.chatID.toString())
+                    .collection('messages')
+                    .orderBy('datetime', descending: true)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  final messages = snapshot.data!.docs;
+
+                  return ListView.builder(
+                    reverse: true,
+                    itemCount: messages.length,
+                    itemBuilder: (context, index) {
+                      final messageData =
+                          messages[index].data() as Map<String, dynamic>;
+
+                      return Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10.0,
+                          horizontal: 15.0,
+                        ),
+                        child: Row(
+                          children: [
+                            if (!messageData['receipt']) ...[
+                              CircleAvatar(
+                                backgroundImage:
+                                    AssetImage(messageData['avatar']),
+                              ),
+                              SizedBox(width: 10.0),
+                            ],
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: messageData['receipt']
+                                    ? CrossAxisAlignment.end
+                                    : CrossAxisAlignment.start,
+                                children: [
+                                  Text(_formatTimestamp(
+                                      messageData['datetime'])),
+                                  SizedBox(height: 5.0),
+                                  Container(
+                                    padding: EdgeInsets.all(16.0),
+                                    decoration: BoxDecoration(
+                                      color: messageData['receipt']
+                                          ? Color.fromARGB(255, 250, 86, 114)
+                                          : Color(0xFF26117A),
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(15.0),
+                                      ),
+                                    ),
+                                    child: Text(
+                                      messageData['message'],
+                                      style: TextStyle(
+                                        color: Color.fromARGB(
+                                          255,
+                                          219,
+                                          219,
+                                          219,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            if (messageData['receipt']) ...[
+                              SizedBox(width: 10.0),
+                              CircleAvatar(
+                                backgroundImage:
+                                    AssetImage(messageData['avatar']),
+                              ),
+                            ],
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
+              height: 60.0,
+              color: Colors.grey[200],
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _messageController,
+                      decoration: InputDecoration(
+                        hintText: "Type your message",
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.send),
+                    onPressed: () {
+                      final message = _messageController.text;
+                      if (message.isNotEmpty) {
+                        // Send message to Firebase
+                        _sendMessage(message);
+                        _messageController.clear();
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String _formatTimestamp(Timestamp timestamp) {
+    DateTime dateTime = timestamp.toDate();
+    return DateFormat.jm().format(dateTime);
+  }
+
+  void _sendMessage(String message) async {
+    final firestore = FirebaseFirestore.instance;
+
+    final messageData = {
+      'message': message,
+      'datetime': Timestamp.now(),
+      'receipt': true,
+      'name': 'Sarah',
+      'avatar': 'assets/Lom.png',
+    };
+
+    await firestore
+        .collection('chat')
+        .doc(widget.chatID.toString())
+        .collection('messages')
+        .add(messageData);
+  }
+}
